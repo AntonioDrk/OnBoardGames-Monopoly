523e78436d5b9d9adf7099b36a676b714e35b7eb
Remus-Antonio <ekro98@gmail.com>
2019-03-20 15:30:56 +0200

    Initial commit of the project

---
+++ Assets/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private int indexPosition = 0; // Indicates the position on the board list (the list of cards that are on the board)
+    private float moveAmount = 1.25f;
+    private Vector3 targetPos;
+    private int amountToMove = 1;
+    private float speedMovement = 20f;
+
+
+    void Start()
+    {
+        targetPos = transform.position;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (amountToMove < 0)
+        {
+            return;
+        }
+
+        for (int i = 1; i <= amountToMove; i++)
+        {
+            Vector3 localForward = transform.worldToLocalMatrix.MultiplyVector(transform.forward);
+            transform.Translate(localForward * i * moveAmount * Time.fixedDeltaTime);
+            //transform.position = Vector3.Lerp(transform.position, localForward * i * moveAmount, Time.fixedDeltaTime);
+            //Vector3.MoveTowards(transform.position, localForward * i * moveAmount);
+            if ((i + indexPosition) % 10 == 0)
+            {
+                //Debug.Log("Sunt in if for some reason");
+                transform.eulerAngles += new Vector3(0, 90, 0);
+            }
+        }
+        indexPosition = (indexPosition + amountToMove) % 40;
+    }
+
+    void updateMovement(int amountToMove)
+    {
+     
+        
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    
+    private Camera mainCamera;
+    // THIS IS TESTING ONLY
+    // This is bad v and needs to be changed ASAP, we will pass the players vector between the scripts
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private Vector3 cameraDistance = new Vector3(0, -2.8f, 2.9f);
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+
+        if(mainCamera == null)
+        {
+            Debug.LogError("Main Camera not found! Make sure it's tagged as main camera");
+        }
+
+        // GET RID OF THIS CHECK AFTER TESTING
+        if(player == null)
+        {
+            Debug.LogError("Target player is not set !!!");
+        }
+    }
+
+    
+    void Update()
+    {
+        UpdatePosCamera(player);
+    }
+
+    // Moves the camera to face the player
+    void UpdatePosCamera(GameObject target)
+    {
+        mainCamera.transform.position = target.transform.position - cameraDistance;
+    }
+}
