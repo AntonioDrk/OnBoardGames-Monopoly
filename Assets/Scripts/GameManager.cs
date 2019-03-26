17ea18c9e401ee6b018dfdc826bbcb9b4d87ccf0
Remus-Antonio <ekro98@gmail.com>
2019-03-26 16:12:46 +0200

    Camera Rotation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -40,6 +40,12 @@ public class GameManager : MonoBehaviour
     // Moves the camera to face the player
     void UpdatePosCamera(GameObject target)
     {
-        mainCamera.transform.position = target.transform.position - cameraDistance;
+        // Get the distance the camera should be from the player (the distance is set in cameraDistance vector)
+        // rotation * cameraDistance makes the camera be placed with respect to thge player rotation, so it always stays "BEHIND" the player
+        mainCamera.transform.position = target.transform.position - target.transform.rotation * cameraDistance;
+
+        // Take the targets rotation on the y axis and apply it to the camera, don't change the cameras x axis rotation
+        mainCamera.transform.eulerAngles = new Vector3(mainCamera.transform.eulerAngles.x, target.transform.eulerAngles.y, 0);
+        
     }
 }
