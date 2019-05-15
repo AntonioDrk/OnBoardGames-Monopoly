558e503eeb48a175ded1e56370511129d0400b73
Calin George-Adrian <calingeorgeadrian98@gmail.com>
2019-05-15 17:06:26 +0300

    Added colors
    Added cardColor property to PropertyCard class and edited Properties.json file to include the color codes

--- Assets/Scripts/PropertyCard.cs
+++ Assets/Scripts/PropertyCard.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class PropertyCard : Card
 {
     public int id;
+    public int[] cardColor = new int[3];
     public string cardName;
     public int priceValue;
     public int mortgageValue;
